f9b49abf587d9bd4cf414f1d8023ba0e0e3fe4b2
Wiillamss <willamsgomes95@gmail.com>
2025-03-18 14:50:15 +0000

    commint

--- Tema3_Xadrex/Mestre/Desafio-Mestre.c
+++ Tema3_Xadrex/Mestre/Desafio-Mestre.c
@@ -2,19 +2,28 @@
 
 
     void movimentoTorre (int casas) {
-        if (casas > 0) {
+        if (casas > 1) {
 
             movimentoTorre (casas - 1);
-            printf("%d - Direita \n", casas);
+            
+    
         }
+        char direcao_Torre[20];
+        printf("Digite a direção de movimentação da Torre (Cima ou Direita)\n");
+        scanf(" %s", direcao_Torre);
+        printf("%d para %s \n", casas, direcao_Torre);
+        
     }
 
     int main (){
         
-        int quantidade = 5;
+        int quantCasas_Torre;
 
+        printf("Digite um número de 1 à 8, representando a quantidade de casas que a Torre vai percorrer: ");
+        scanf("%d", &quantCasas_Torre);
+        
         printf("*** Realizando o movimento da Torre\n");
-        movimentoTorre(quantidade);
+        movimentoTorre(quantCasas_Torre);
 
         return 0;
     }
